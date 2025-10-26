623c30f22399cbed65cdc86d28a798d9f2f06f64
Marcel <marwozniak01@gmail.com>
2025-10-26 18:04:32 +0100

    accesibility improvements

--- 05-tenzies/Die.jsx
+++ 05-tenzies/Die.jsx
@@ -3,5 +3,15 @@ export default function Die(props) {
     backgroundColor: props.isHeld ? "#59E391" : "white",
   };
 
-  return <button style={styles}>{props.value}</button>;
+  return (
+    <button
+      style={styles}
+      onClick={props.hold}
+      aria-pressed={props.isHeld}
+      aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+    >
+      {props.value}
+    </button>
+  );
 }
