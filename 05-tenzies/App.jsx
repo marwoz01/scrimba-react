588b3c9e427eb920aca545ad14f7b9ad02458f92
Marcel <marwozniak01@gmail.com>
2025-10-26 14:49:00 +0100

    change dice to objects

--- 05-tenzies/App.jsx
+++ 05-tenzies/App.jsx
@@ -5,19 +5,21 @@ export default function App() {
   const [dice, setDice] = useState(generateAllNewDice());
 
   function generateAllNewDice() {
-    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    return new Array(10).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    }));
   }
 
   function rollDice() {
     setDice(generateAllNewDice());
   }
 
-  const diceElements = dice.map((num) => <Die value={num} />);
+  const diceElements = dice.map((dieObj) => <Die value={dieObj.value} />);
 
   return (
     <main>
       <div className="dice-container">{diceElements}</div>
-
       <button className="roll-dice" onClick={rollDice}>
         Roll
       </button>
